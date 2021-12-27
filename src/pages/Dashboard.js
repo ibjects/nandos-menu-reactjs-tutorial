b548e5aa85659c77658be4d4c3bf4c3f5cc463aa
Chaudhry Talha <talha440@gmail.com>
2021-12-27 11:09:03 +0800

    Add Menu Item Implemented

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,67 @@
-import React, { useState } from 'react';
-import { Table, Card, Image, Button, Modal, Form, FloatingLabel } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Table, Card, Image, Button, Modal, Form, FloatingLabel, Spinner } from 'react-bootstrap';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+import FirestoreService from '../utils/services/FirestoreService';
+import NotLoggedInView from '../components/NoLoggedInView';
 
 
 function Dashboard(props) {
 
-    const [showAddEditForm, setShowAddEditForm] = useState(true);
+    const [user, setUser] = useState(null);
+    const [menuItems, setMenuItems] = useState([]);
+    const [menuCategories, setMenuCategories] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [currentMenuItem, setCurrentMenuItem] = useState({
+        "itemName": '',
+        "itemCategory": '',
+        "itemPrice": 0
+    })
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            setUser(user);
+        } else {
+            setUser(null);
+        }
+    })
+
+    function fetchMenuCategories() {
+        setIsLoading(true);
+        FirestoreService.getAllMenuCategories().then((response) => {
+            setIsLoading(false);
+            setMenuCategories(response._delegate._snapshot.docChanges);
+        }).catch((e) => {
+            setIsLoading(false);
+            alert("Error occured while fetching the menu categories. " + e);
+        })
+    }
+
+    function fetchMenuItems() {
+        setIsLoading(true);
+        FirestoreService.getAllMenuItems().then((response) => {
+            setIsLoading(false);
+            setMenuItems(response._delegate._snapshot.docChanges);
+        }).catch((e) => {
+            setIsLoading(false);
+            alert("Error occured while fetching the menu item. " + e);
+        })
+    }
+
+    useEffect(() => {
+        if (user !== null) {
+            if (menuCategories.length <= 0) {
+                fetchMenuCategories();
+            }
+            fetchMenuItems();
+        }
+    }, [user])
+
+    const [showAddEditForm, setShowAddEditForm] = useState(false);
     const [addEditFormType, setAddEditFormType] = useState('Add'); //Add, Edit
     const [validated, setValidated] = useState(false);
 
@@ -13,10 +70,29 @@ function Dashboard(props) {
     const handleModalClose = () => {
         setShowAddEditForm(false);
         setShowDeleteDialogue(false);
+        setIsLoading(false);
     }
 
     const handleAddEditFormSubmit = (e) => {
-        alert("Functionality Coming Soon");
+        e.preventDefault();
+        const { itemName, itemCategory, itemPrice } = e.target.elements;
+
+        if (itemPrice.value && itemName.value) {
+            if (addEditFormType === "Add") {
+                setIsLoading(true);
+                FirestoreService.AddNewMenuItem(itemName.value, itemCategory.value, itemPrice.value).then(() => {
+                    alert(`${itemName.value} is successfully added to the menu.`)
+                    setCurrentMenuItem({ "itemName": '', "itemCategory": '', "itemPrice": 0 })
+                    handleModalClose();
+                    window.location.reload(false);
+                }).catch((e) => {
+                    alert("Error occured: " + e.message);
+                    setIsLoading(false);
+                })
+            }
+        }
+
+        setValidated(true)
     }
 
     const handleMenuItemDelete = (e) => {
@@ -25,103 +101,123 @@ function Dashboard(props) {
 
     return (
         <>
-            {/* Add/Edit Form START */}
-            <Modal show={showAddEditForm} onHide={handleModalClose}>
-                <Form noValidate validated={validated} onSubmit={handleAddEditFormSubmit}>
-                    <Modal.Header>
-                        <Modal.Title>{(addEditFormType === 'Add') ? 'Add Menu Item' : 'Edit'}</Modal.Title>
+            {/* <h1>You're not logged in. Please <a href="/login">login</a> first then come to dashboard.</h1> */}
+            {(user === null) && <NotLoggedInView />}
+            {(isLoading === true) && <Spinner animation="border" variant="secondary" />}
+            {(user !== null) && <>
+                {/* Add/Edit Form START */}
+                <Modal show={showAddEditForm} onHide={handleModalClose}>
+                    <Form noValidate validated={validated} onSubmit={handleAddEditFormSubmit}>
+                        <Modal.Header>
+                            <Modal.Title>{(addEditFormType === 'Add') ? 'Add Menu Item' : 'Edit'}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <FloatingLabel controlId="itemName" label="Item Name" className="mb-3" >
+                                <Form.Control required type='text' placeholder='Enter item name' size='md' value={currentMenuItem?.itemName} onChange={(e) => {
+                                    setCurrentMenuItem({
+                                        "itemName": (e.target.value) ? e.target.value : '',
+                                        "itemCategory": currentMenuItem?.itemCategory,
+                                        "itemPrice": currentMenuItem?.itemPrice
+                                    })
+                                }} />
+                                <Form.Control.Feedback type='invalid'>Item name is required</Form.Control.Feedback>
+                            </FloatingLabel>
+
+                            <FloatingLabel controlId="itemCategory" label="Item Category" className="mb-3" >
+                                <Form.Select value={currentMenuItem?.itemCategory} onChange={(e) => {
+                                    setCurrentMenuItem({
+                                        "itemName": currentMenuItem?.itemName,
+                                        "itemCategory": e.target.value,
+                                        "itemPrice": currentMenuItem?.itemPrice
+                                    })
+                                }}>
+                                    {(menuCategories) && (menuCategories.map((menuCategory, index) => (
+                                        // catNum.integerValue
+                                        <option key={index} value={menuCategory.doc.data.value.mapValue.fields.catName.stringValue}>
+                                            {menuCategory.doc.data.value.mapValue.fields.catName.stringValue}</option>
+                                    )))}
+                                </Form.Select>
+                            </FloatingLabel>
+
+                            <FloatingLabel controlId="itemPrice" label="Price (MYR)" className="mb-3">
+                                <Form.Control required type='text' placeholder='Enter item price' size='md' value={currentMenuItem?.itemPrice} onChange={(e) => {
+                                    setCurrentMenuItem({
+                                        "itemName": currentMenuItem?.itemName,
+                                        "itemCategory": currentMenuItem?.itemCategory,
+                                        "itemPrice": parseFloat((e.target.value) ? e.target.value : 0)
+                                    })
+                                }} />
+                                <Form.Control.Feedback type='invalid'>Item Price is required</Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button type="submit">{(addEditFormType === 'Add') ? 'Add' : 'Update'}</Button>
+                        </Modal.Footer>
+                    </Form>
+                </Modal>
+                {/* Add/Edit Form END */}
+
+                {/* Delete Confirmation Dialogue START */}
+                <Modal show={showDeleteDialogue} onHide={handleModalClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Delete Menu Item</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-
-                        <FloatingLabel controlId="itemName" label="Item Name" className="mb-3" >
-                            <Form.Control required type='text' placeholder='Enter item name' size='md' />
-                            <Form.Control.Feedback type='invalid'>Item name is required</Form.Control.Feedback>
-                        </FloatingLabel>
-
-                        <FloatingLabel controlId="itemCategory" label="Item Category" className="mb-3" >
-                            <Form.Select>
-                                <option value="0">Starters & Snacks</option>
-                                <option value="1">Salads</option>
-                                <option value="2">Peri-Peri Chicken</option>
-                                <option value="3">Sharing Platters</option>
-                            </Form.Select>
-                        </FloatingLabel>
-
-                        <FloatingLabel controlId="itemPrice" label="Price" className="mb-3" >
-                            <Form.Control required type='text' placeholder='Enter item price' size='md' />
-                            <Form.Control.Feedback type='invalid'>Item Price is required</Form.Control.Feedback>
-                        </FloatingLabel>
-
+                        <p>Are you sure you want to delete this menu item?</p>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button type="submit">{(addEditFormType === 'Add') ? 'Add' : 'Update'}</Button>
+                        <Button variant="secondary" onClick={handleModalClose}>Cancel</Button>
+                        <Button variant="danger" onClick={handleMenuItemDelete}>Yes, Delete</Button>
                     </Modal.Footer>
-                </Form>
-            </Modal>
-            {/* Add/Edit Form END */}
+                </Modal>
+                {/* Delete Confirmation Dialogue END */}
 
-            {/* Delete Confirmation Dialogue START */}
-            <Modal show={showDeleteDialogue} onHide={handleModalClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Delete Menu Item</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>Are you sure you want to delete this menu item?</p>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleModalClose}>Cancel</Button>
-                    <Button variant="danger" onClick={handleMenuItemDelete}>Yes, Delete</Button>
-                </Modal.Footer>
-            </Modal>
-            {/* Delete Confirmation Dialogue END */}
-
-            <Card style={{ margin: 24 }}>
-                <Card.Header className="d-flex justify-content-between align-items-center">
-                    <div className="align-items-center" style={{ marginRight: 8 }}>
-                        <Image src={'https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Nandos_logo.svg/1200px-Nandos_logo.svg.png'} style={{ width: 80 }} />
-                        <h4 style={{ marginTop: 8, }}>Dashboard</h4>
-                    </div>
-                    <Button style={{ backgroundColor: '#000', borderWidth: 0, }}>Add New Item</Button>
-                </Card.Header>
-                <Card.Body>
-                    <Table responsive>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Item Name</th>
-                                <th>Category</th>
-                                <th>Price (MYR)</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Cheessy Garlic Pita</td>
-                                <td>Starters & Snacks</td>
-                                <td>4</td>
-                                <td>
-                                    <Button variant='primary'>✎ Edit</Button>{' '}
-                                    <Button variant='danger'>x Delete</Button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Hummus with  PERi-PERi Drizzle & Pita</td>
-                                <td>Starters & Snacks</td>
-                                <td>5.5</td>
-                                <td>
-                                    <Button variant='primary'>✎ Edit</Button>{' '}
-                                    <Button variant='danger'>x Delete</Button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </Card.Body>
-                <Card.Footer className="d-flex justify-content-between align-items-center">
-                    <p style={{ marginTop: 8, fontSize: 12, color: '#A1A1A1' }}>Nandos Menu v1.0.0 - <a href="/login">Logout</a></p>
-                </Card.Footer>
-            </Card>
+                <Card style={{ margin: 24 }}>
+                    <Card.Header className="d-flex justify-content-between align-items-center">
+                        <div className="align-items-center" style={{ marginRight: 8 }}>
+                            <Image src={'https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Nandos_logo.svg/1200px-Nandos_logo.svg.png'} style={{ width: 80 }} />
+                            <h4 style={{ marginTop: 8, }}>Dashboard</h4>
+                        </div>
+                        <Button style={{ backgroundColor: '#000', borderWidth: 0, }} onClick={() => {
+                            setShowAddEditForm(true);
+                        }}>Add New Item</Button>
+                    </Card.Header>
+                    <Card.Body>
+                        <Table responsive>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Item Name</th>
+                                    <th>Category</th>
+                                    <th>Price (MYR)</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {(menuItems) && (menuItems.map((menuItem, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{menuItem.doc.data.value.mapValue.fields.itemName.stringValue}</td>
+                                        <td>{menuItem.doc.data.value.mapValue.fields.itemCategory.stringValue}</td>
+                                        <td>{menuItem.doc.data.value.mapValue.fields.itemPrice.doubleValue ? menuItem.doc.data.value.mapValue.fields.itemPrice.doubleValue : menuItem.doc.data.value.mapValue.fields.itemPrice.integerValue}</td>
+                                        <td>
+                                            <Button variant='primary' onClick={() => {
+                                                alert("Edit functionality coming soon")
+                                            }}>✎ Edit</Button>{' '}
+                                            <Button variant='danger' onClick={() => {
+                                                alert("Delete functionality coming soon")
+                                            }}>x Delete</Button>
+                                        </td>
+                                    </tr>
+                                )))}
+                            </tbody>
+                        </Table>
+                    </Card.Body>
+                    <Card.Footer className="d-flex justify-content-between align-items-center">
+                        <p style={{ marginTop: 8, fontSize: 12, color: '#A1A1A1' }}>Nandos Menu v1.0.0 - <a href="/login">Logout</a></p>
+                    </Card.Footer>
+                </Card>
+            </>}
         </>
     );
 }
--- src/utils/services/FirestoreService.js
+++ src/utils/services/FirestoreService.js
@@ -1,13 +1,40 @@
 import { db } from '../firestore'
 
 function getAllMenuItems() {
-    return new Promise((response, reject) => {
+    return new Promise((resolve, reject) => {
         db.collection("MenuItems").get().then((allMenuItems) => {
-            response(allMenuItems);
+            resolve(allMenuItems);
         }).catch((e) => {
             reject(e);
         })
     })
 }
 
-export default { getAllMenuItems }
+function getAllMenuCategories() {
+    return new Promise((resolve, reject) => {
+        db.collection("MenuCategories").get().then((allMenuCategories) => {
+            resolve(allMenuCategories);
+        }).catch((e) => {
+            reject(e);
+        })
+    })
+}
+
+function AddNewMenuItem(itemName, itemCategory, itemPrice) {
+    return new Promise((resolve, reject) => {
+        const data = {
+            "itemName": itemName,
+            "itemCategory": itemCategory,
+            "itemPrice": parseFloat(itemPrice)
+        }
+
+        db.collection("MenuItems").add(data).then((docRef) => {
+            resolve(docRef);
+        }).catch((e) => {
+            reject(e);
+        })
+
+    })
+}
+
+export default { getAllMenuItems, getAllMenuCategories, AddNewMenuItem }
